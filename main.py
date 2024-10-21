0f2805b3c56d4a0838547448e427252ba2223ebf
uantero <uantero@iddei.com>
2024-10-21 23:14:53 +0200

    Measurements centered on the robot

--- main.py
+++ main.py
@@ -2,6 +2,30 @@
 ####   USE GROVER TO:
 ####      Find x,y in a grid
 ####
+
+## Calculated in IBM
+"""
+
+        0 1 2 3 4 5
+    0 ['0 0 1 1 0 0']
+    1 ['1 0 0 0 0 0']
+    2 ['0 0 0 0 0 0']
+    3 ['1 0 0 1 0 1']
+    4 ['1 1 0 0 0 0']
+    5 ['0 0 0 1 0 0']
+
+   Job: cwb9b3wjyrs0008wyzt0
+   21/10/2024:
+
+    ROWS: {'0100': 188, '1100': 188, '1001': 186, '0101': 184, '1000': 179, '1110': 179, '0001': 179, '0010': 178, '0111': 176, '0110': 175, '1101': 174, '0011': 170, '0000': 168, '1111': 167, '1011': 157, '1010': 152}
+    COLS: {'11': 804, '10': 747, '01': 646, '00': 603}
+
+   Result: 
+     · ROW: 3
+     · COL: 3
+
+
+"""
  
  
 #initialization
@@ -42,23 +66,6 @@ from lib import execute_on_IONQ, execute_on_IBM, execute_on_QuantumInspire
 
 """
 inp_map_string = [
-    ["
-    col={}
-    for each in 0 0 0 0 0 0 0 0"] ,
-    ["0 0 X 0 0 0 0 0"] ,
-    ["0 0 0 0 0 0 0 0"] ,
-    ["0 0 0 0 0 0 0 0"] ,
-    ["0 0 0 X 0 0 0 0"] ,
-    ["0 0 X 0 X 0 0 0"] ,
-    ["0 0 0 X 0 0 0 0"] ,
-    ["0 0 0 0 0 0 0 0"]     
-]
-"""
-
-#############################################
-##  ----------- GLOBALS --------------------
-# THE MAP
-inp_map_string = [
     ["0 0 1 1 0 0"] ,
     ["1 0 0 0 0 0"] ,
     ["0 0 0 0 0 0"] ,
@@ -68,14 +75,28 @@ inp_map_string = [
  
 ]
 
+"""
+
+#############################################
+##  ----------- GLOBALS --------------------
+# THE MAP
+inp_map_string = [
+    ["0 0 1 0 "] ,
+    ["0 1 0 1 "] ,
+    ["0 0 1 0 "] ,
+    ["0 0 1 0 "] ,
+]
+
 for each in inp_map_string:
     print (each)
 print (" ")
 
 
 # ROBOT'S SENSORS (horizontal & vertical)
-inp_pattern_row=  ["1", "0", "1"] # row ?
-inp_pattern_col=  ["1", "0", "1"] # col ?
+# A single data is centered in the robot
+# From there... if row length is 2, each data is shown with the robot in the middle-->   1 r 2 
+inp_pattern_row=  ["1", "1"]#, "0"] # row ?
+inp_pattern_col=  ["1"] # col ?
 
 inp_map_string_joined = (  ("".join(["".join(each) for each in inp_map_string])).replace(" ","") )
 inp_pattern_row_joined = "".join(inp_pattern_row)
@@ -85,7 +106,7 @@ inp_pattern_col_joined = "".join(inp_pattern_col)
 BYTE_SIZE = len(inp_pattern_row[0]) # 1 / 2 / ... bits ?
 
 # Send it to an external provider?
-MAKE_IT_REAL = True
+MAKE_IT_REAL = False
 
 # Which external provider?
 # OPTIONS: IONQ, IBM, QUANTUMINSPIRE
@@ -147,7 +168,7 @@ def create_map_search(inp_map_string, inp_pattern_row, row_elements, inp_pattern
 
     inp_pattern_row = [inp_pattern_row[i:i + BYTE_SIZE] for i in range(0, len(inp_pattern_row), BYTE_SIZE)] 
     inp_pattern_col = [inp_pattern_col[i:i + BYTE_SIZE] for i in range(0, len(inp_pattern_col), BYTE_SIZE)] 
-    print (inp_pattern_row)
+    #print (inp_pattern_row)
 
 
     #print ("ROWS: %s" %rows)
@@ -162,31 +183,51 @@ def create_map_search(inp_map_string, inp_pattern_row, row_elements, inp_pattern
             self.compare_to=compare_to
             self.compare_to_str=compare_to_str
         def __repr__(self):
-            return " (%s|%s[%s|%s]<%s>) " %(self.row, self.col, self.type, self.element, self.compare_to)
-
-    for each_row_index in range(len(rows[0])-len(col_elements)+1):
+            return " (%s|%s[%s|%s]<%s|%s>) " %(self.row, self.col, self.type, self.element, self.compare_to, self.compare_to_str)
+    
+    for each_row_index in range( math.floor(len(row_elements)/2),  len(rows) -  len(row_elements) +1 ):    
+        #print ("ROW! %s" %each_row_index)
         if each_row_index>=len(rows):
             continue
-        this_row=rows[each_row_index]        
-        for each_column_index in range(len(this_row)-len(row_elements)+1):
-            temp_positions=[]
+        this_row=rows[each_row_index]                
+        for each_column_index in range( math.floor(len(col_elements)/2),  len(columns) -  math.floor(len(col_elements))+1):    
+            #print ("COL! %s" %each_column_index)
+            temp_positions=[]            
+            start_col_positions = [each for each in range(-math.floor(len(row_elements)/2), math.floor(len(row_elements)/2))]            
             for each_row_bit in range(len(row_elements)):
-                #print("Row: %s, Col: %s" %(each_row_index, each_column_index+each_row_bit))
-                element=Element(each_row_index, each_column_index+each_row_bit, 
-                        rows[each_row_index][each_column_index+each_row_bit],
+                if each_column_index+each_row_bit+start_col_positions[each_row_bit]>=len(rows[each_row_index]):
+                    continue
+
+                
+                element=Element(
+                        each_row_index, 
+                        each_column_index, 
+                        rows[each_row_index][each_column_index+each_row_bit+start_col_positions[each_row_bit]],
                         "row",
                         inp_pattern_row[each_row_bit],
                         row_elements[each_row_bit]
                         )
+                #print("Row: %s, Col: %s --> %s" %(each_row_index, each_column_index, row_elements[each_row_bit]))
                 temp_positions.append(element)
 
+            start_row_positions = [each for each in range(-math.floor(len(col_elements)/2), math.floor(len(col_elements)/2))]            
             if len(inp_pattern_col)>1:
                 for each_col_bit in range(len(col_elements)):
-                    #print("*Row: %s, Col: %s" %(each_row_index+each_col_bit, each_column_index))
+                    if each_row_index+each_col_bit+start_row_positions[each_col_bit]>=len(rows):
+                        continue
+
+                    #print (".....")
+                    #print (each_row_index)
+                    #print (each_col_bit)
+                    #print (start_row_positions[each_col_bit])
+
+                    #print("*Row: %s, Col: %s" %(each_row_index, each_column_index))
                     if each_row_index+each_col_bit>=len(rows):
                         continue
-                    element=Element(each_row_index+each_col_bit, each_column_index, 
-                                rows[each_row_index+each_col_bit][each_column_index],
+                    element=Element(
+                                each_row_index, 
+                                each_column_index, 
+                                rows[each_row_index+each_col_bit+start_row_positions[each_col_bit]][each_column_index],
                                 "col",
                                 inp_pattern_col[each_col_bit],
                                 col_elements[each_col_bit]
@@ -221,16 +262,18 @@ print ("JOINED MAP: %s" %inp_map_string_joined)
 print ("JOINED inp_pattern_row_joined: %s" %inp_pattern_row_joined)
 
 #SEARCH SPACE:
-desired_row=2
-desired_col=2
+desired_row=1
+desired_col=1
 format_string = "{:0" + str(int(len(search_space)/2)) + "b}" # /2 because we have here row and col    
 formated_searchspace = "%s%s" %(format_string.format(desired_row), format_string.format(desired_col))
 print ("desired search_space: %s" %formated_searchspace)
 print ("MAP: %s" %inp_map_string_joined)
 
 set_inputs(qc, inp_map_string_joined, map )
-initialize_H(qc, search_space)
-#set_inputs(qc, formated_searchspace, search_space)
+if not TEST_ORACLE:
+    initialize_H(qc, search_space)
+else:
+    set_inputs(qc, formated_searchspace, search_space)
 set_inputs(qc, inp_pattern_row_joined, search_row)
 set_inputs(qc, inp_pattern_col_joined, search_col)
 
@@ -244,6 +287,7 @@ logger.info("Number of qubits: %s" %(qc.num_qubits) )
 
 logger.info("Map has %s possible options (N=%s)" %(len(positions), len(positions)))
 
+print (positions)
 #checkEqual(qc, [map[3]], [search_row[0]], "1", temporary, ancilla, output, search_space)
 
 def oracle(qc, search_space, positions, temporary, ancilla, output):
@@ -280,7 +324,7 @@ logger.info("Looking at: (%s)" %search_space)
 logger.info("Looking for (row): (%s)" %inp_pattern_row)
 logger.info("Looking for (col): (%s)" %inp_pattern_col)
 
-TEST_ORACLE=False
+
 if not TEST_ORACLE:
     N=len(positions)
     M=2
@@ -330,6 +374,6 @@ if not TEST_ORACLE:
 else:
     oracle(qc, search_space, positions, temporary, ancilla, output)
     add_measurement(qc, output, "res1")
-    counts=simulate(qc, num_shots=600)
+    counts=simulate(qc, num_shots=200)
     print (counts)
 
